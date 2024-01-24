c57132172812ce75a85e5cf352b446095003fd5b
User-PC <a>
2024-01-24 11:57:55 +0700

    perulangan dengan for dan while

---
+++ perulangan dengan for.py
@@ -0,0 +1,22 @@
+""""
+perulangan latihan
+"""
+
+jumlah_buku = 100
+jumlah_buku_yang_sudah_dibaca = 0
+
+print ('Baca bukumu')
+
+print (f'Jumlah buku yang sudah saya baca {jumlah_buku_yang_sudah_dibaca}')
+
+for jumlah_buku_yang_sudah_dibaca in range(1,jumlah_buku+1):
+    print(f'Jumlah buku yang sudah saya baca {jumlah_buku_yang_sudah_dibaca} ')
+
+print()
+print (f'Total buku yang sudah saya baca {jumlah_buku_yang_sudah_dibaca}')
+
+
+
+
+
+
---
+++ perulangan dengan while.py
@@ -0,0 +1,23 @@
+""""
+perulangan dengan while
+"""
+
+jumlah_buku = 10
+
+print ('Baca semua bukumu dengan while')
+
+jumlah_buku_yang_sudah_dibaca = 0
+print (f'Jumlah buku yang sudah saya baca {jumlah_buku_yang_sudah_dibaca}')
+
+while jumlah_buku_yang_sudah_dibaca < jumlah_buku:
+    jumlah_buku_yang_sudah_dibaca = jumlah_buku_yang_sudah_dibaca+1
+    print(f'Jumlah buku yang sudah saya baca {jumlah_buku_yang_sudah_dibaca} ')
+
+print()
+print (f'Total buku yang sudah saya baca {jumlah_buku_yang_sudah_dibaca}')
+
+
+
+
+
+
